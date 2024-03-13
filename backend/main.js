f79a041658cda944ff61bf1c6f279b6e13bfff7f
Baatarvant <baatarvant@gmail.com>
2024-03-13 16:33:38 +0800

    find user by email query

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const transactionRouter = require("./routers/transactionRouter");
+const usersRouter = require("./routers/usersRouter");
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/transactions", transactionRouter);
+app.use("/users", usersRouter);
 
 const dbUsername = "erka@pinecone.mn";
 const dbPassword = "12345678";
